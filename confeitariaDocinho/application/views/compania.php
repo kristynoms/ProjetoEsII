3ecf1e63e0fe7c75233559e1d83d2769a41adeb3
Kristy <kristyrec@gmail.com>
2013-05-03 08:39:09 -0300

    Modificações na compania

--- confeitariaDocinho/application/views/compania.php
+++ confeitariaDocinho/application/views/compania.php
@@ -1,1 +1,39 @@
-<h2>Compania titulo</h2>
+<div id="compania">
+<h2>Um Pouco da História</h2>
+<br>
+Desde 1986 a <i>CONFEITARIA DOCINHO</i> vem oferecendo, na cidade de Porto Alegre, os<br>
+mais delicados sabores com diversidade e refinamento em seus produtos.<br>
+São doces, salgados e tortas aperfeiçoados cuidadosamente <br>
+por uma equipe que tem o prazer de aplicar experiência e muito amor</br > 
+na criação e desenvolvimento de seus produtos<br> com ingredientes da melhor qualidade e matérias-primas naturais,</br >
+buscando sempre satisfazer aqueles que são a sua motivação: os <i>Clientes</i>.<br> 
+A empresa também oferece serviços como Café da Manhã,<br>
+Almoço, Chá Capital, além de Kits festa e escritório com a opção de serviços de garçons.</br > 
+E atendento a solicitações, criamos o sistema de encomendas online,</br>
+trazendo mais agilidade e conforto para suas festas e comemorações.</br >
+As receitas recebem a atenção e o toque artesanal exigidos por alimentos que degustamos nos momentos</br > 
+mais prazerosos da vida, o resultado?</br > Só experimentando...!</br>
+<br>
+ 
+<h2>Missão:</h2>
+Fabricar produtos alimentícios de alta qualidade,</br >
+Oferecer produtos e serviços de qualidade através de um atendimento personalizado,</br >
+Consolidar a marca <i>CONFEITARIA DOCINHO</i> no mercado,</br >
+Criar um ambiente agradável entre clientes e colaboradores,</br >
+Ter lojas dinâmicas e modernas,</br >
+Ter qualidade do pré ao pós vendas,</br >
+Oferecer soluções que atendam e excedam as expectativas dos clientes,</br >
+Desenvolver soluções para atender as novas demandas.</br>
+
+ 
+<h2>Visão:</h2>
+<ul>
+<li>Compromisso com o cliente</li>
+<li>Pessoas comprometidas e realizadas</li>
+<li>Qualidade em tudo que faz</li>
+<li>Empreendorismo responsável</li>
+<li>Inovação</li>
+<li>Crescimento e rentabilidade</li>
+</ul>
+
+</div>
